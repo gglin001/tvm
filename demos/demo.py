ca964a7dabe8c549a9abe00a69882c2b8b0406c5
songlin.guo <songlin.guo@nextvpu.co>
2021-05-20 16:51:00 +0800

    add a demo

---
+++ demos/demo.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+# repo_path/python
+tvm_path = '/data/songlin/repos/tvm/python'
+sys.path.append(tvm_path)
+import tvm
+
+print(os.getgid())
+print(tvm.__file__)
+
+x = tvm.tir.Var("x", "int32")
+
+# set breakpoint ast here and releated c++ code
+# `src/tir/ir/expr.cc`
+# `tir.Add`
+y = tvm.tir.Add(x, x)
+print(y)
